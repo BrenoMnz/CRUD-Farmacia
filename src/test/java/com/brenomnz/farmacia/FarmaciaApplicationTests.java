e4188f515365b1b668b1fd5807d1cbb5d649e7f5
Breno Menezes <breno.luizmenezes0123@gmail.com>
2024-01-04 10:49:00 -0300

    Configuracoes iniciais realizadas

---
+++ src/test/java/com/brenomnz/farmacia/FarmaciaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brenomnz.farmacia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FarmaciaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
